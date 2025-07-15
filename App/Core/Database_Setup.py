22d70d10adb2d3a21a466cd88cad11c341849aa7
Zaynab Ashrafi <zynbashrfi@gmail.com>
2025-07-15 12:52:39 +0330

    typo corrected (DATATIME -> DATETIME)

--- App/Core/Database_Setup.py
+++ App/Core/Database_Setup.py
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS messages (
     sender_id INTEGER NOT NULL,
     receiver_id INTEGER NOT NULL, 
     content TEXT NOT NULL,
-    timestamp DATATIME DEFAULT CURRENT_TIMESTAMP,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY(sender_id) REFERENCES users(id),
     FOREIGN KEY(receiver_id) REFERENCES users(id)
 )
